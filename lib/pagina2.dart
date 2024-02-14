54d28a947d402219e5211b375815d337493c9dab
luis jose sanchez carrasco <luissancar@yahoo.es>
2024-02-14 13:27:00 +0100

    uno

---
+++ lib/pagina2.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Pagina2 extends StatelessWidget {
+
+  final String textoEnviado;
+  const Pagina2(this.textoEnviado,{super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Página 2"),
+        ),
+        body: Container(
+            color: Colors.deepOrange,
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Column(
+              children: [
+                Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      textoEnviado,
+                      textAlign: TextAlign.center,
+                      style:
+                      TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    )),
+                ElevatedButton(
+                    onPressed: () => {Navigator.pop(context)},
+                    child: Text("Regresar",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold)))
+              ],
+            )));
+  }
+}
